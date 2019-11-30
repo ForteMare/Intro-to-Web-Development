f83867afa1ce597baea8e9bb71523569efc6a20e
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-11-30 23:00:02 +0100

    part two of the lecture added

--- Coursera/lec53a copy/js/script.js
+++ Coursera/lec53a copy/js/script.js
@@ -1,5 +1,4 @@
 // Document Object Model (DOM) Manipulation
 console.log(document.getElementById("title"));      
 
-// Displays true on Google console 
 console.log(document instanceof HTMLDocument);
