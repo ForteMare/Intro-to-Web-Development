486914d445624f79355f339700dc6d9dce20ed81
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-12-01 13:54:49 +0100

    reduced lecture 53 to one folder

--- Coursera/lec53a/js/script.js
+++
@@ -1,5 +0,0 @@
-// Document Object Model (DOM) Manipulation
-console.log(document.getElementById("title"));      
-
-// Displays true on Google console 
-console.log(document instanceof HTMLDocument);
