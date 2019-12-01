32b7319de0f460b217be163cbf5c0d83f5c84260
ForteMare <30961251+ForteMare@users.noreply.github.com>
2019-12-01 14:59:02 +0100

    part one of week 5 finished

--- Coursera/lec55/js/script.js
+++ Coursera/lec55/js/script.js
@@ -1,6 +1,8 @@
 // Event handling
 document.addEventListener("DOMContentLoaded", function(event) {
   function sayHello() {
+    console.log(event);
+
     this.textContent = "Said it!";
     var name = document.getElementById("name").value; // Value is for accessin the property value
     var message = "<h2>Hello " + name + "!</h2>";
@@ -17,6 +19,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 
   document.querySelector("button").addEventListener("click", sayHello);
+
+  document.querySelector("body").addEventListener("mousemove", function(event) {
+    if (event.shiftKey === true) {
+      console.log("x: " + event.clientX);
+      console.log("y: " + event.clientY);
+    }
+  });
 });
 
 // // Unobstrusive event binding
